4e9644ea740c0b233738f9dc60603038d159b3ac
Ganesh Govind J <codemuscle@Ganeshs-MacBook-Air.local>
2023-06-09 14:26:27 +0530

    product-page initial

--- src/core/components/Meta.js
+++ src/core/components/Meta.js
@@ -1,10 +1,11 @@
 export default function Meta() {
   return (
     <>
-      <title>Epic Saas</title>
+      <title>Digital Hive</title>
       <meta
         name="description"
         content="All the most epic things on the internet, all in one place."
+        favicon="/assets/confetti.png"
       />
     </>
   );
